5c9ee90fd16593222559b9af77fe1a168d4a9c50
westw0w <kostya.ribnikov@gmail.com>
2026-09-20 01:49:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Max_Chat/',
   plugins: [react()],
   resolve: {
     alias: {
